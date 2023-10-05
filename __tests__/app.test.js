ecdd6ece15c2a4505ece5543a60997acfa6bd39e
PeteJCoding <petejohncoding@gmail.com>
2023-10-05 14:36:04 +0100

    updated changes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -40,6 +40,30 @@ describe("GET /api/topics", () => {
 });
 
 describe("GET /api", () => {
+    it("should return JSON object with endpoints details", async () => {
+        const response = await request(app).get("/api");
+        expect(response.status).toEqual(200);
+
+        const { body } = response;
+        const topicsEndPointObject = body.endPointData["GET /api/topics"];
+
+        expect(typeof topicsEndPointObject).toEqual("object");
+        expect(topicsEndPointObject).toHaveProperty(
+            "description",
+            expect.any(String)
+        );
+        expect(topicsEndPointObject).toHaveProperty(
+            "queries",
+            expect.any(Array)
+        );
+        expect(topicsEndPointObject).toHaveProperty(
+            "exampleResponse",
+            expect.any(Object)
+        );
+    });
+});
+
+describe("GET /api/articles", () => {
     it("should return a 200 status code", async () => {
         const response = await request(app).get("/api/articles");
         expect(response.status).toBe(200);
